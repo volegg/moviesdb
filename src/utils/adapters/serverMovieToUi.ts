87884581b5c1708c85579f1775629636516ecdf2
Oleg Voskovich <oleg.voskovich@pawatech.com>
2024-09-19 12:02:06 +0300

    Added empty poster

--- src/utils/adapters/serverMovieToUi.ts
+++ src/utils/adapters/serverMovieToUi.ts
@@ -1,9 +1,11 @@
 const prefixMovieDbUrl = "https://media.themoviedb.org/t/p/w220_and_h330_face";
 
 export function serverMovieToUi(movie: ServerMovieProps): Movie {
+    const posterPath = movie.poster_path ? movie.poster_path : movie.backdrop_path;
+
     return {
         title: movie.title,
-        image: prefixMovieDbUrl + movie.poster_path,
+        image: posterPath ? prefixMovieDbUrl + posterPath : "emptyPoster.jpeg",
         date: movie.release_date,
         rating: ((movie.vote_average * 10) >> 0) / 10,
         lang: movie.original_language,
